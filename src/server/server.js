46c86a205e61fa183c83e436411bcb10ab9010a7
Murilo Ito <murilo.ito@mercadobitcoin.com.br>
2019-11-21 17:18:20 -0300

    Add server features

--- src/server/server.js
+++ src/server/server.js
@@ -1,29 +1,74 @@
-import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
-import Config from './config.json';
-import Web3 from 'web3';
-import express from 'express';
+import FlightSuretyApp from "../../build/contracts/FlightSuretyApp.json";
+import Config from "./config.json";
+import Web3 from "web3";
+import express from "express";
 
+const STATUS_CODES = [0, 10, 20, 30, 40, 50];
 
-let config = Config['localhost'];
-let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
-web3.eth.defaultAccount = web3.eth.accounts[0];
-let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+const NUM_ORACLES = 25;
 
+let config = Config["localhost"];
+let web3 = new Web3(
+  new Web3.providers.WebsocketProvider(config.url.replace("http", "ws"))
+);
 
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
-});
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+deal();
 
 const app = express();
-app.get('/api', (req, res) => {
-    res.send({
-      message: 'An API for use with your Dapp!'
-    })
-})
+
+app.get("/api", (req, res) => {
+  res.send({
+    message: "An API for use with your Dapp!"
+  });
+});
+
+async function deal() {
+  const accounts = await web3.eth.getAccounts();
+  web3.eth.defaultAccount = accounts[0];
+
+  let flightSuretyApp = new web3.eth.Contract(
+    FlightSuretyApp.abi,
+    config.appAddress
+  );
+
+  const fee = await flightSuretyApp.methods.REGISTRATION_FEE().call();
+
+  for (let i = 0; i < NUM_ORACLES; i++) {
+    await flightSuretyApp.methods
+      .registerOracle()
+      .send({ value: fee, from: web3.eth.defaultAccount, gas: 3000000 });
+  }
+
+  flightSuretyApp.events.OracleRequest(
+    {
+      fromBlock: 0
+    },
+    async (error, event) => {
+      if (error) {
+        console.log(error);
+      } else {
+        for (let i = 0; i < NUM_ORACLES; i++) {
+          let indexes = await flightSuretyApp.methods.getMyIndexes(i).call();
+          if (indexes.indexOf(event.returnValues.index) >= 0) {
+            const pos = getRndInteger(0, STATUS_CODES.length - 1);
+            await flightSuretyApp.methods
+              .submitOracleResponse(
+                event.returnValues.index,
+                event.returnValues.airline,
+                event.returnValues.flight,
+                event.returnValues.timestamp,
+                STATUS_CODES[pos]
+              )
+              .send({ from: web3.eth.defaultAccount, gas: 5000000 });
+          }
+        }
+      }
+    }
+  );
+}
 
 export default app;
-
-
